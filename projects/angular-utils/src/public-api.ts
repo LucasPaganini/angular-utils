eb749f238bdb55bf4eab8b191c1b7b14266a5ff5
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-03-27 19:47:45 +0000

    Delete unused auto generated files

--- projects/angular-utils/src/public-api.ts
+++ projects/angular-utils/src/public-api.ts
@@ -2,7 +2,4 @@
  * Public API Surface of angular-utils
  */
 
-export * from './lib/angular-utils.component';
-export * from './lib/angular-utils.module';
-export * from './lib/angular-utils.service';
 export * from './lib/unsubscriber';
