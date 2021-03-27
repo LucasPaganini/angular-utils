b59bb1d13dc48b01ce33fa21f81a3051e0bffaa9
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-03-27 21:48:08 +0000

    Format

--- src/unsubscriber/unsubscriber.service.ts
+++ src/unsubscriber/unsubscriber.service.ts
@@ -1,3 +1,11 @@
+/*************************************************************************
+ * Copyright (c) 2021–present Lucas Paganini. All rights reserved.
+ * Contact: <me@lucaspaganini.com>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE file for details.
+ **************************************************************************/
+
 import { Injectable, OnDestroy } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
