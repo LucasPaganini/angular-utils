c7222c47c84a060daeae27dccb00833a00dd0d6f
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-07-12 18:30:27 -0300

    Format

--- src/intersection-observer/index.ts
+++ src/intersection-observer/index.ts
@@ -1,2 +1,10 @@
+/*************************************************************************
+ * Copyright (c) 2021–present Lucas Paganini. All rights reserved.
+ * Contact: <me@lucaspaganini.com>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE file for details.
+ **************************************************************************/
+
 export * from './intersection-observer.directive'
 export * from './intersection-observer.module'
